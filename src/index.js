9b8323d9576be8fc61f056ad6d66708dcda7eb9d
David Jegat <david.jegat@gmail.com>
2022-02-28 14:25:13 +0100

    Ajout de request et nodemon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Coucou les amis')
+import fastify from 'fastify'
+
+const app = fastify({ logger: true })
+
+app.get('/hello', {}, () => {
+  return 'Bonjour à tous !'
+})
+
+app.listen(4646, '0.0.0.0')
