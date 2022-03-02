4a4a76dfb375b9ad8f8007ce2915a70dd5e7773c
David Jegat <david.jegat@gmail.com>
2022-03-02 11:24:02 +0100

    Ajout des corrections d'exo pour les schèmas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import fastify from 'fastify'
 import mongo from 'mongodb'
-import { NewBookSchema, BookSchema } from './schemas/books-schema.js'
+import {
+  UpdateBookSchema,
+  BookSchema,
+  BookCollectionSchema,
+  NewBookSchema,
+} from './schemas/books-schema.js'
 
 /**
  * Definission d'un fonction de démarrage asynchrone
@@ -17,29 +22,49 @@ async function start() {
 
   // EXO 1
   // Création d'une route qui récupére tout les livres
-  app.get('/books', {}, async () => {
-    // Récupération de plusieurs livre. Cette instruction
-    // nous retourne un tableaux de livre (un objet avec
-    // tout les champs du livre)
-    const livres = await db.collection('books').find().toArray()
+  app.get(
+    '/books',
+    {
+      schema: {
+        response: {
+          200: BookCollectionSchema,
+        },
+      },
+    },
+    async () => {
+      // Récupération de plusieurs livre. Cette instruction
+      // nous retourne un tableaux de livre (un objet avec
+      // tout les champs du livre)
+      const livres = await db.collection('books').find().toArray()
 
-    return livres
-  })
+      return livres
+    },
+  )
 
   // EXO 2
   // Création d'une route qui récupére un seul livre
-  app.get('/books/:id', {}, async request => {
-    // On demande à un récupérer un seul livre
-    const livre = await db.collection('books').findOne({
-      // On récupére le livre par son ID. Attention
-      // à bien envoyé l'identifiant dans la fonction
-      // mongo.ObjectId
-      _id: mongo.ObjectId(request.params.id),
-    })
+  app.get(
+    '/books/:id',
+    {
+      schema: {
+        response: {
+          200: BookSchema,
+        },
+      },
+    },
+    async request => {
+      // On demande à un récupérer un seul livre
+      const livre = await db.collection('books').findOne({
+        // On récupére le livre par son ID. Attention
+        // à bien envoyé l'identifiant dans la fonction
+        // mongo.ObjectId
+        _id: mongo.ObjectId(request.params.id),
+      })
 
-    // On retourne le livre
-    return livre
-  })
+      // On retourne le livre
+      return livre
+    },
+  )
 
   // EXO 3
   // Création d'une route pour créer un nouveau livre
@@ -68,33 +93,54 @@ async function start() {
 
   // EXO 4
   // Création d'une route pour modifier un livre
-  app.patch('/books/:id', {}, async request => {
-    const body = request.body
+  app.patch(
+    '/books/:id',
+    {
+      schema: {
+        body: UpdateBookSchema,
+        response: {
+          200: BookSchema,
+        },
+      },
+    },
+    async request => {
+      const body = request.body
 
-    await db
-      .collection('books')
-      .updateOne({ _id: mongo.ObjectId(request.params.id) }, { $set: body })
+      await db
+        .collection('books')
+        .updateOne({ _id: mongo.ObjectId(request.params.id) }, { $set: body })
 
-    const livre = await db.collection('books').findOne({
-      _id: mongo.ObjectId(request.params.id),
-    })
+      const livre = await db.collection('books').findOne({
+        _id: mongo.ObjectId(request.params.id),
+      })
 
-    return livre
-  })
+      return livre
+    },
+  )
 
   // EXO 5
   // Création d'une route pour supprimer un livre
-  app.delete('/books/:id', {}, async request => {
-    const livre = await db.collection('books').findOne({
-      _id: mongo.ObjectId(request.params.id),
-    })
+  app.delete(
+    '/books/:id',
+    {
+      schema: {
+        response: {
+          200: BookSchema,
+        },
+      },
+    },
+    async request => {
+      const livre = await db.collection('books').findOne({
+        _id: mongo.ObjectId(request.params.id),
+      })
 
-    await db.collection('books').deleteOne({
-      _id: mongo.ObjectId(request.params.id),
-    })
+      await db.collection('books').deleteOne({
+        _id: mongo.ObjectId(request.params.id),
+      })
 
-    return livre
-  })
+      return livre
+    },
+  )
 
   // On lance le serveur logique sur le port 4646
   app.listen(4646, '0.0.0.0')
--- src/schemas/books-schema.js
+++ src/schemas/books-schema.js
@@ -1,13 +1,20 @@
 import S from 'fluent-json-schema'
 
 /**
- * Définition du schèma pour un nouveau livre
+ * Définition du schèma pour une mise à jour d'un livre
+ */
+export const UpdateBookSchema = S.object()
+  .additionalProperties(false)
+  .prop('title', S.string())
+  .prop('description', S.string())
+  .prop('price', S.number())
+
+/**
+ * Definition du schèma pour la création d'un livre
  */
 export const NewBookSchema = S.object()
-  .additionalProperties(false)
-  .prop('title', S.string().required())
-  .prop('description', S.string().required())
-  .prop('price', S.number().required())
+  .required(['title', 'description', 'price'])
+  .extend(UpdateBookSchema)
 
 /**
  * Définition du schèma d'un livre
@@ -15,3 +22,8 @@ export const NewBookSchema = S.object()
 export const BookSchema = S.object()
   .prop('_id', S.string().required())
   .extend(NewBookSchema)
+
+/**
+ * Définition du schèma d'une collection de livre
+ */
+export const BookCollectionSchema = S.array().items(BookSchema)
